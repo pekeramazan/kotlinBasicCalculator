414a3f4ab74fe3743ac66591b7690ebc4eb57fa4
ramazan <ramazan@2a-grup.com>
2020-03-29 17:07:53 +0300

    end

--- app/src/main/java/com/ramazanpeker/kotlinbasiccalculator/MainActivity.kt
+++ app/src/main/java/com/ramazanpeker/kotlinbasiccalculator/MainActivity.kt
@@ -3,9 +3,11 @@ package com.ramazanpeker.kotlinbasiccalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-var input1=0
-    var input=0
+var input1 : Int?=null
+    var input2 : Int? = null
+    var resultValue :Int? =null
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,20 +15,66 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
     fun sum(view: View)
     {
+        input1= number1.text.toString().toIntOrNull()
+        input2= number2.text.toString().toIntOrNull()
+        if(input1==null || input2==null)
+        {
+            result.text="Error!"
+        }
+        else
+        {
+            resultValue=input1!!+input2!!
+            result.text= "Result : $resultValue"
+        }
+
 
     }
     fun sub(view: View)
     {
+        input1= number1.text.toString().toIntOrNull()
+        input2= number2.text.toString().toIntOrNull()
+        if(input1==null || input2==null)
+        {
+            result.text="Error!"
+        }
+        else
+        {
+            resultValue=input1!!-input2!!
+            result.text= "Result : $resultValue"
+        }
 
     }
     fun multiply(view: View)
     {
+        input1= number1.text.toString().toIntOrNull()
+        input2= number2.text.toString().toIntOrNull()
+        if(input1==null || input2==null)
+        {
+            result.text="Error!"
+        }
+        else
+        {
+            resultValue=input1!!*input2!!
+            result.text= "Result : $resultValue"
+        }
 
     }
     fun dive(view: View)
     {
+        input1= number1.text.toString().toIntOrNull()
+        input2= number2.text.toString().toIntOrNull()
+        if(input1==null || input2==null)
+        {
+            result.text="Error!"
+        }
+        else
+        {
+            resultValue=input1!!/input2!!
+            result.text= "Result : $resultValue"
+        }
 
     }
 
