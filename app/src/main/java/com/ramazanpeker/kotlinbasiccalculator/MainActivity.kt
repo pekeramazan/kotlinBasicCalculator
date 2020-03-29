b710424381fa4d808b0e64e155f353613937d5df
ramazan <ramazan@2a-grup.com>
2020-03-29 16:57:44 +0300

    first

--- app/src/main/java/com/ramazanpeker/kotlinbasiccalculator/MainActivity.kt
+++ app/src/main/java/com/ramazanpeker/kotlinbasiccalculator/MainActivity.kt
@@ -2,6 +2,10 @@ package com.ramazanpeker.kotlinbasiccalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
+var input1=0
+    var input=0
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +13,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun sum(view: View)
+    {
+
+    }
+    fun sub(view: View)
+    {
+
+    }
+    fun multiply(view: View)
+    {
+
+    }
+    fun dive(view: View)
+    {
+
+    }
+
 }
